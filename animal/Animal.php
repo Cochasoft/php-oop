4177b976241397e6337ef2581d8a5f08b15a0d2c
Kevin-Jhonatan <jhonatan72700606@gmail.com>
2021-05-06 15:55:41 -0400

    nuevo proyecto animal

---
+++ animal/Animal.php
@@ -0,0 +1,14 @@
+<?php
+class Animal
+{
+    var $patas = 4;
+    function __construct()
+    {
+        $this->patas;
+    }
+    function getPatas()
+    {
+        return $this->patas;
+    }
+}
+?>
